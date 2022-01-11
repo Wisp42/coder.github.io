c899f168f0b2a146d0b80ac97af959fed2166d94
Wisp <mr.wisp173.1.1@gmail.com>
2022-01-11 18:12:20 +0200

    1.0

---
+++ scripts.js
@@ -0,0 +1,157 @@
+let inputText = document.getElementById('input').value;
+let backValue = '';
+let backFunc = function(){
+	console.log(backValue)
+	document.getElementById('input').value = backValue;
+}
+let copyValue = function(){
+	document.getElementById('input').select();
+	document.execCommand("copy");
+}
+
+let MD5 = function(d){var r = M(V(Y(X(d),8*d.length)));return r.toLowerCase()};function M(d){for(var _,m="0123456789ABCDEF",f="",r=0;r<d.length;r++)_=d.charCodeAt(r),f+=m.charAt(_>>>4&15)+m.charAt(15&_);return f}function X(d){for(var _=Array(d.length>>2),m=0;m<_.length;m++)_[m]=0;for(m=0;m<8*d.length;m+=8)_[m>>5]|=(255&d.charCodeAt(m/8))<<m%32;return _}function V(d){for(var _="",m=0;m<32*d.length;m+=8)_+=String.fromCharCode(d[m>>5]>>>m%32&255);return _}function Y(d,_){d[_>>5]|=128<<_%32,d[14+(_+64>>>9<<4)]=_;for(var m=1732584193,f=-271733879,r=-1732584194,i=271733878,n=0;n<d.length;n+=16){var h=m,t=f,g=r,e=i;f=md5_ii(f=md5_ii(f=md5_ii(f=md5_ii(f=md5_hh(f=md5_hh(f=md5_hh(f=md5_hh(f=md5_gg(f=md5_gg(f=md5_gg(f=md5_gg(f=md5_ff(f=md5_ff(f=md5_ff(f=md5_ff(f,r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+0],7,-680876936),f,r,d[n+1],12,-389564586),m,f,d[n+2],17,606105819),i,m,d[n+3],22,-1044525330),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+4],7,-176418897),f,r,d[n+5],12,1200080426),m,f,d[n+6],17,-1473231341),i,m,d[n+7],22,-45705983),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+8],7,1770035416),f,r,d[n+9],12,-1958414417),m,f,d[n+10],17,-42063),i,m,d[n+11],22,-1990404162),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+12],7,1804603682),f,r,d[n+13],12,-40341101),m,f,d[n+14],17,-1502002290),i,m,d[n+15],22,1236535329),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+1],5,-165796510),f,r,d[n+6],9,-1069501632),m,f,d[n+11],14,643717713),i,m,d[n+0],20,-373897302),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+5],5,-701558691),f,r,d[n+10],9,38016083),m,f,d[n+15],14,-660478335),i,m,d[n+4],20,-405537848),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+9],5,568446438),f,r,d[n+14],9,-1019803690),m,f,d[n+3],14,-187363961),i,m,d[n+8],20,1163531501),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+13],5,-1444681467),f,r,d[n+2],9,-51403784),m,f,d[n+7],14,1735328473),i,m,d[n+12],20,-1926607734),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+5],4,-378558),f,r,d[n+8],11,-2022574463),m,f,d[n+11],16,1839030562),i,m,d[n+14],23,-35309556),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+1],4,-1530992060),f,r,d[n+4],11,1272893353),m,f,d[n+7],16,-155497632),i,m,d[n+10],23,-1094730640),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+13],4,681279174),f,r,d[n+0],11,-358537222),m,f,d[n+3],16,-722521979),i,m,d[n+6],23,76029189),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+9],4,-640364487),f,r,d[n+12],11,-421815835),m,f,d[n+15],16,530742520),i,m,d[n+2],23,-995338651),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+0],6,-198630844),f,r,d[n+7],10,1126891415),m,f,d[n+14],15,-1416354905),i,m,d[n+5],21,-57434055),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+12],6,1700485571),f,r,d[n+3],10,-1894986606),m,f,d[n+10],15,-1051523),i,m,d[n+1],21,-2054922799),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+8],6,1873313359),f,r,d[n+15],10,-30611744),m,f,d[n+6],15,-1560198380),i,m,d[n+13],21,1309151649),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+4],6,-145523070),f,r,d[n+11],10,-1120210379),m,f,d[n+2],15,718787259),i,m,d[n+9],21,-343485551),m=safe_add(m,h),f=safe_add(f,t),r=safe_add(r,g),i=safe_add(i,e)}return Array(m,f,r,i)}function md5_cmn(d,_,m,f,r,i){return safe_add(bit_rol(safe_add(safe_add(_,d),safe_add(f,i)),r),m)}function md5_ff(d,_,m,f,r,i,n){return md5_cmn(_&m|~_&f,d,_,r,i,n)}function md5_gg(d,_,m,f,r,i,n){return md5_cmn(_&f|m&~f,d,_,r,i,n)}function md5_hh(d,_,m,f,r,i,n){return md5_cmn(_^m^f,d,_,r,i,n)}function md5_ii(d,_,m,f,r,i,n){return md5_cmn(m^(_|~f),d,_,r,i,n)}function safe_add(d,_){var m=(65535&d)+(65535&_);return(d>>16)+(_>>16)+(m>>16)<<16|65535&m}function bit_rol(d,_){return d<<_|d>>>32-_};
+let SHA1 = function(str) {
+  var hash
+  try {
+    var crypto = require('crypto')
+    var sha1sum = crypto.createHash('sha1')
+    sha1sum.update(str)
+    hash = sha1sum.digest('hex')
+  } catch (e) {
+    hash = undefined
+  }
+  if (hash !== undefined) {
+    return hash
+  }
+  var _rotLeft = function (n, s) {
+    var t4 = (n << s) | (n >>> (32 - s))
+    return t4
+  }
+  var _cvtHex = function (val) {
+    var str = ''
+    var i
+    var v
+    for (i = 7; i >= 0; i--) {
+      v = (val >>> (i * 4)) & 0x0f
+      str += v.toString(16)
+    }
+    return str
+  }
+  var blockstart
+  var i, j
+  var W = new Array(80)
+  var H0 = 0x67452301
+  var H1 = 0xEFCDAB89
+  var H2 = 0x98BADCFE
+  var H3 = 0x10325476
+  var H4 = 0xC3D2E1F0
+  var A, B, C, D, E
+  var temp
+  // utf8_encode
+  str = unescape(encodeURIComponent(str))
+  var strLen = str.length
+  var wordArray = []
+  for (i = 0; i < strLen - 3; i += 4) {
+    j = str.charCodeAt(i) << 24 |
+      str.charCodeAt(i + 1) << 16 |
+      str.charCodeAt(i + 2) << 8 |
+      str.charCodeAt(i + 3)
+    wordArray.push(j)
+  }
+  switch (strLen % 4) {
+    case 0:
+      i = 0x080000000
+      break
+    case 1:
+      i = str.charCodeAt(strLen - 1) << 24 | 0x0800000
+      break
+    case 2:
+      i = str.charCodeAt(strLen - 2) << 24 | str.charCodeAt(strLen - 1) << 16 | 
+0x08000
+      break
+    case 3:
+      i = str.charCodeAt(strLen - 3) << 24 |
+        str.charCodeAt(strLen - 2) << 16 |
+        str.charCodeAt(strLen - 1) <<
+      8 | 0x80
+      break
+  }
+  wordArray.push(i)
+  while ((wordArray.length % 16) !== 14) {
+    wordArray.push(0)
+  }
+  wordArray.push(strLen >>> 29)
+  wordArray.push((strLen << 3) & 0x0ffffffff)
+  for (blockstart = 0; blockstart < wordArray.length; blockstart += 16) {
+    for (i = 0; i < 16; i++) {
+      W[i] = wordArray[blockstart + i]
+    }
+    for (i = 16; i <= 79; i++) {
+      W[i] = _rotLeft(W[i - 3] ^ W[i - 8] ^ W[i - 14] ^ W[i - 16], 1)
+    }
+    A = H0
+    B = H1
+    C = H2
+    D = H3
+    E = H4
+    for (i = 0; i <= 19; i++) {
+      temp = (_rotLeft(A, 5) + ((B & C) | (~B & D)) + E + W[i] + 0x5A827999) & 
+0x0ffffffff
+      E = D
+      D = C
+      C = _rotLeft(B, 30)
+      B = A
+      A = temp
+    }
+    for (i = 20; i <= 39; i++) {
+      temp = (_rotLeft(A, 5) + (B ^ C ^ D) + E + W[i] + 0x6ED9EBA1) & 0x0ffffffff
+      E = D
+      D = C
+      C = _rotLeft(B, 30)
+      B = A
+      A = temp
+    }
+    for (i = 40; i <= 59; i++) {
+      temp = (_rotLeft(A, 5) + ((B & C) | (B & D) | (C & D)) + E + W[i] + 
+0x8F1BBCDC) & 0x0ffffffff
+      E = D
+      D = C
+      C = _rotLeft(B, 30)
+      B = A
+      A = temp
+    }
+    for (i = 60; i <= 79; i++) {
+      temp = (_rotLeft(A, 5) + (B ^ C ^ D) + E + W[i] + 0xCA62C1D6) & 0x0ffffffff
+      E = D
+      D = C
+      C = _rotLeft(B, 30)
+      B = A
+      A = temp
+    }
+    H0 = (H0 + A) & 0x0ffffffff
+    H1 = (H1 + B) & 0x0ffffffff
+    H2 = (H2 + C) & 0x0ffffffff
+    H3 = (H3 + D) & 0x0ffffffff
+    H4 = (H4 + E) & 0x0ffffffff
+  }
+  temp = _cvtHex(H0) + _cvtHex(H1) + _cvtHex(H2) + _cvtHex(H3) + _cvtHex(H4)
+  return temp.toLowerCase()
+}
+let Base64 = function(d){return(btoa(d))};
+let ASCII = function(d){
+	let r = '';
+	if(inputText.length > 0){
+		for(let i = 0; i < inputText.length; i++){
+			r += d.charCodeAt(i)
+		}
+	}
+	return(r)
+};
+// console.log(inputText)
+
+function inputListener(func_name){
+	inputText = document.getElementById('input').value;
+	backValue = inputText;
+	console.log(func_name(inputText));
+	document.getElementById('input').value = func_name(inputText)
+}
